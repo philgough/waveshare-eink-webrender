6c9cdc0374d9363aa144cbf99563cc39a903359e
Phil Gough <phillip.gough@sydney.edu.au>
2022-08-28 22:11:49 +1000

    updates to website

--- waveshare.py
+++ waveshare.py
@@ -2,27 +2,54 @@ from flask import Flask, render_template
 
 from datetime import datetime
 
+# url requests
 import requests
 import json
 
 
-# import sys
+# weather
 from weather_au import api
 
-from weather_au import summary
+
+
+
+
+
+
+
 
 app = Flask(__name__)
+
+def nth(d):
+    if d == 1:
+        return 'st'
+    elif d == 2:
+        return 'nd'
+    elif d == 3:
+        return 'rd'
+    elif d > 3 and d < 21:
+        return 'th'
+    else:
+        return 'th'
+
+
 
 @app.route('/')
 def index():
 
 
 
+    day = str(datetime.now().day) + nth(datetime.now().day % 10)
+    
+
 
     dates={ 'day': datetime.now().day,
-            'month': datetime.now().month,
+            'nth': nth(datetime.now().day % 10),
+            'month': datetime.now().strftime("%b"),
             'year': datetime.now().year
     }
+
+
 
     # options = { method: 'GET', headers: { Accept: 'application/json' } };
     x = requests.get('https://beta.ourmanna.com/api/v1/get?format=json&order=daily')
@@ -36,26 +63,17 @@ def index():
     print(w)
     location = w.location()
 
-    print(f"\nLocation: {location['name']} {location['state']}, timezone:{location['timezone']}\n")
+    obs = w.observations()
+    today = w.forecasts_daily()[0]
 
-    for warn in w.warnings():
-        print(f"Warning short title:  {warn['short_title']}")
+    weather = {
+        'temp_now': today['now']['temp_now'],
+        'rain_chance': today['rain']['chance'],
+        'temp_min': today['temp_min'],
+        'temp_max': today['temp_max'],
+        'icon_descriptor': today['icon_descriptor']
+    }
 
-        warning = w.warning(id=warn['id'])
-        print(f"Warning title:        {warning['title']}")
-
-
-    # print(f"\nObservations (temp): {observations['temp']:2}")
-    # print(f"Forecast Rain:       amount:{forecast_rain['amount']}, chance:{forecast_rain['chance']}")
-
-    # print('\n3 Hourly:')
-    # for f in w.forecasts_3hourly():
-        # print(f"{f['time']} temp:{f['temp']:2}, {f['icon_descriptor']}")
-
-    # print(summary.Summary(search='strathfield+nsw'))
-
-    observations = w.observations()
-    forecast_rain = w.forecast_rain()
-    weather = {'temp':observations['temp'],'rain':{'amount':{forecast_rain['amount']}, 'chance':{forecast_rain['chance']}}}
+    print(weather)
 
     return render_template('index.html', dates=dates, verse=verse, weather=weather)
