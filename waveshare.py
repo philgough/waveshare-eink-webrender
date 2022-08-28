d1146f65b179772921517c409e910895ee576d91
Phillip Gough <phillip.gough@sydney.edu.au>
2022-08-28 23:11:53 +1000

    updates

--- waveshare.py
+++ waveshare.py
@@ -12,6 +12,40 @@ from weather_au import api
 
 
 
+from ics import Calendar 
+import requests
+
+def cal_from_file(filename: str, prodid: str = 'PRODID:-//placeholder//text//EN\n') -> Calendar:
+    buf = []
+
+    with open(filename, 'r') as f:
+        i = 0
+        vcal = -1
+        missing_prodid = True
+
+        for line in f:
+            buf.append(line)
+
+            if line == "BEGIN:VCALENDAR\n":
+                vcal = i
+                missing_prodid = True
+            elif line == "END:VCALENDAR\n":
+                if missing_prodid:
+                    #print(f'VCALENDAR without PRODID on line {vcal} in {filename}.')
+                    buf[vcal] = buf[vcal] + prodid
+                vcal = -2
+            elif vcal != -1 and missing_prodid and line.startswith("PRODID:"):
+                missing_prodid = False
+            i += 1
+
+        if vcal == -1:
+            raise TypeError(f'{filename} is not an ics file') # FIXME
+        elif vcal >= 0 and missing_prodid:
+            #print(f'VCALENDAR without PRODID nor end on line {vcal} in {filename}.')
+            buf[vcal] = buf[vcal] + prodid
+
+    return Calendar(''.join(buf))
+
 
 
 
@@ -75,5 +109,13 @@ def index():
     }
 
     print(weather)
+    url =''
+    with open('cal_url.txt', 'r') as f:
+        url=f.readline().strip()
+    # print(url)
+    response = requests.get(url)
+    open("cal.ics", "wb").write(response.content)
+    # print(cal_from_file('cal.ics'))
+    
+    return render_template('index.html', dates=dates, verse=verse, weather=weather, )
 
-    return render_template('index.html', dates=dates, verse=verse, weather=weather)
