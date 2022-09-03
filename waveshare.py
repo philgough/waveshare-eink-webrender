4b8c4f75cd9392000d570d04b9c489fa34ce709f
Phillip Gough <phillip.gough@sydney.edu.au>
2022-09-03 16:49:08 +1000

    update to make it actually run

--- waveshare.py
+++ waveshare.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 
 from datetime import datetime
-
-# url requests
+import calendar
 import requests
 import json
 
@@ -45,6 +44,50 @@ def cal_from_file(filename: str, prodid: str = 'PRODID:-//placeholder//text//EN\
             buf[vcal] = buf[vcal] + prodid
 
     return Calendar(''.join(buf))
+
+# bearer_token = os.environ.get("BEARER_TOKEN")
+bearer_token = ''
+with open('twitter.txt') as t_file:
+    bearer_token = t_file.readline().strip()
+    print(bearer_token)
+
+def create_url(list_id):
+# Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    tweet_fields = "tweet.fields=lang,author_id"
+    # Be sure to replace list-id with any List ID
+    # id = "list-id"
+    url = "https://api.twitter.com/2/lists/{}/tweets".format(list_id)
+    return url, tweet_fields
+
+
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2ListLookupPython"
+    return r
+
+
+def connect_to_endpoint(url, list_fields):
+    response = requests.request("GET", url, auth=bearer_oauth, params=list_fields)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
 
 
 
@@ -121,10 +164,53 @@ def index():
 
     events = []
     for e in list(cal.timeline.start_after(datetime.now().astimezone())):
-        print("Event '{}' starting {}".format(e.name, e.begin.humanize()))
+        print(e.begin.weekday())
+        # print("Event '{}' starting {}".format(e.name, e.begin.humanize()))
         events.append({
             'name': e.name,
-            'h_begin': e.begin.humanize()
+            'h_begin': e.begin.humanize(),
+            'begin': calendar.day_name[e.begin.weekday()]
         })
-    return render_template('index.html', dates=dates, verse=verse, weather=weather, events=events, len_events=len(events))
+
+    ## twitter
+    # list_id = 1565329082333954048
+
+    # url = "https://api.twitter.com/2/lists/{}".format(id)
+    
+    # search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+    # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+    # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+    # query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
+    # search_url =  'https://api.twitter.com/1.1/trends/closest.json'
+    # query_params = {'query': 'lat=37.781157,long=-122.400612831116'}
+
+    # tweet = requests.get(search_url, auth=bearer_oauth, params=query_params)
+    # print(tweet.status_code)
+    # if tweet.status_code != 200:
+    #     raise Exception(tweet.status_code, tweet.text)
+
+    # print(tweet.text)
+    
+    # return response.json()
+    # json_response = response.json()
+    # json_response = connect_to_endpoint(search_url, query_params)
+    # print(json.dumps(json_response, indent=4, sort_keys=True))
+    
+    # print(search_url)
+
+    waveshare_twitter_list_id = '1565329082333954048'
+    url, list_fields = create_url(waveshare_twitter_list_id)
+    json_response = connect_to_endpoint(url, list_fields)
+    # print(json.dumps(json_response, indent=4, sort_keys=True))
+    # print(json_response['data'][0])
+    news = []
+    for response in json_response['data']:
+        # print(response)
+        t = response['text'].split('http')[0][:140]
+        if len(response['text'].split('http')[0]) > 139:
+            t += '...'
+        news.append(t)
+
+    return render_template('index.html', dates=dates, verse=verse, weather=weather, events=events, len_events=len(events), news=news, len_news=len(news))
 
